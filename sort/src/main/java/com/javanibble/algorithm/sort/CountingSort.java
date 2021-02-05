9e018649e585c63081a9a001f9ff087506bd44b6
javanibble <javanibble@gmail.com>
2021-02-05 12:14:47 +0100

    Update Counting Sort

--- sort/src/main/java/com/javanibble/algorithm/sort/CountingSort.java
+++ sort/src/main/java/com/javanibble/algorithm/sort/CountingSort.java
@@ -1,27 +1,4 @@
-/**
- * MIT License
- * 
- * Copyright (c) 2018 André Maré
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-package com.code2bits.algorithm.sort;
+package com.javanibble.algorithm.sort;
 
 
 /**
@@ -31,28 +8,23 @@ package com.code2bits.algorithm.sort;
  * arithmetic to determine the position of each key. This algorithm does not make use of comparisons to sort 
  * the values. In simplistic terms, the algorithm counts the number of occurrences of each value in order to 
  * sort it.
- * 
- * @author  André Maré
  */
 public final class CountingSort {
-
     
     public void sort(int[] collection) {
         if (collection != null) {
             int maxValue = findMaxValue(collection);
             countingSort(collection, maxValue);
         } else {
-            throw new IllegalArgumentException("Input paramenter for array to sort is null.");
+            throw new IllegalArgumentException("Input parameter for array to sort is null.");
         }
     }
-    
     
     private void countingSort(int[] collection, int maxValue) {
         int[] countArray = countOccurrences(collection, maxValue);
         reconstructArray(collection, countArray, maxValue);
     }
-    
-    
+
     private int findMaxValue(int[] collection) {
         int highest = collection[0];
         for (int index = 1; index < collection.length; index ++) {
@@ -62,8 +34,7 @@ public final class CountingSort {
         }
         return highest;
     }
-    
-    
+
     private int[] countOccurrences(int[] collection, int maxValue) {
         int[] tempArray = new int[maxValue + 1];
 
@@ -74,8 +45,7 @@ public final class CountingSort {
         
         return tempArray;
     }
-    
-    
+
     private void reconstructArray(int[] collection, int[] countArray, int maxValue) {
         int j = 0;
         for (int i = 0; i <= maxValue; i++) {
