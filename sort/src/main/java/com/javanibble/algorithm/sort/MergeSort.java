2d2143906ec54327b078c4317cfe4ca8c317c352
javanibble <javanibble@gmail.com>
2021-02-05 12:17:39 +0100

    Update Merge Sort

--- sort/src/main/java/com/javanibble/algorithm/sort/MergeSort.java
+++ sort/src/main/java/com/javanibble/algorithm/sort/MergeSort.java
@@ -1,27 +1,4 @@
-/**
- * MIT License
- * 
- * Copyright (c) 2017 André Maré
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-package com.code2bits.algorithm.sort;
+package com.javanibble.algorithm.sort;
 
 
 /**
@@ -38,12 +15,9 @@ package com.code2bits.algorithm.sort;
  * phase the two arrays are merged so that the elements are sorted within the new larger sorted array. This 
  * process is repeated until a single sorted array is left. This is not an in-place algorithm as temporary 
  * arrays are used.
- * 
- * @author  André Maré
  */
 public final class MergeSort {
 
-    
     public void sort(int[] collection) {
         if (collection != null) {
             mergeSort(collection, 0 , collection.length);
@@ -51,7 +25,7 @@ public final class MergeSort {
             throw new IllegalArgumentException("Input paramenter for array to sort is null.");
         }
     }
-    
+
     
     public  void mergeSort(int[] collection, int minIndex, int maxIndex) {
         if (maxIndex - minIndex < 2) {
@@ -61,10 +35,9 @@ public final class MergeSort {
         int centre = (minIndex + maxIndex) / 2;
         mergeSort(collection, minIndex, centre);
         mergeSort(collection, centre, maxIndex);
-        mergeBack(collection, minIndex, centre, maxIndex);    
-        
+        mergeBack(collection, minIndex, centre, maxIndex);
     }
-    
+
     
     public void mergeBack(int[] collection, int minIndex, int centre, int maxIndex) {
         if (collection[centre - 1] <= collection[centre]) {
