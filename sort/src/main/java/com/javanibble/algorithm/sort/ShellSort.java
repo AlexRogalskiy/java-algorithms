d16d330e683592ce59260d217526896bceb4a1b9
javanibble <javanibble@gmail.com>
2021-02-05 12:20:00 +0100

    Update Shell Sort

--- sort/src/main/java/com/javanibble/algorithm/sort/ShellSort.java
+++ sort/src/main/java/com/javanibble/algorithm/sort/ShellSort.java
@@ -1,27 +1,4 @@
-/**
- * MIT License
- * 
- * Copyright (c) 2018 André Maré
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-package com.code2bits.algorithm.sort;
+package com.javanibble.algorithm.sort;
 
 
 /**
@@ -29,8 +6,6 @@ package com.code2bits.algorithm.sort;
  * is an in-place comparison sort algorithm. Shell Sort is a generalization of insertion sort that allows 
  * the exchange of items that are far apart. The algorithm starts by sorting pairs of elements far apart from
  * each other. The algorithm progressively reduce the gap between elements to be compared.
- * 
- * @author  André Maré
  */
 public final class ShellSort {
 
