b2db8853380c903df74bfe070f7fbbe906d9653a
javanibble <javanibble@gmail.com>
2021-02-05 12:16:47 +0100

    Update Insertion Sort Recursive

--- sort/src/main/java/com/javanibble/algorithm/sort/InsertionSortRecursive.java
+++ sort/src/main/java/com/javanibble/algorithm/sort/InsertionSortRecursive.java
@@ -1,27 +1,4 @@
-/**
- * MIT License
- * 
- * Copyright (c) 2017 André Maré
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-package com.code2bits.algorithm.sort;
+package com.javanibble.algorithm.sort;
 
 
 /**
@@ -31,11 +8,8 @@ package com.code2bits.algorithm.sort;
  * sorted.
  * 
  * The InsertionSortRecursive class implements the InsertionSort algorithm by making use of recursion.
- * 
- * @author	André Maré
  */
 public final class InsertionSortRecursive {
-	
 	
     public void sort(int[] collection) {
         if (collection != null) {
@@ -43,14 +17,12 @@ public final class InsertionSortRecursive {
         } else {
             throw new IllegalArgumentException("Input parameter for array to sort is null.");
         }
-    } 
-
+    }
 
 	private void insertionSort(int[] collection) {
 	    int arrayLength = collection.length;
 	    insertionSort(collection, arrayLength);
 	}
-
 	
 	private void insertionSort(int[] collection, int recursiveValue) {
 	    if (recursiveValue <= 1) {
@@ -69,5 +41,4 @@ public final class InsertionSortRecursive {
 	    collection[j+1] = last;
 	}
 
-	
 }
